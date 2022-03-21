cf2c43cc42e471f289d2234c70d3fa7d644b84e6
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2022-03-21 11:40:56 -0400

    adds add function to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`The result is ${result}`);
+}
+
+add(3, 5);
+add(1, 2);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-let errorMessage = "An error \n" + "occurred";
-outputResult(currentResult, errorMessage);
+
+outputResult(currentResult, calculationDescription);
